644e913d0839f9c1f347566f02e4974476420f74
RunningToy <github@mihatsch.cc>
2015-11-27 15:36:04 +0100

    Update getWeather.php
    Show Map with Devices which were selected

--- getWeather.php
+++ getWeather.php
@@ -2,11 +2,19 @@
 error_reporting(E_ALL ^ E_NOTICE);
 
 //Abfrage der NetATMO Public Sensoren um die lokale Ist-Temperatur zu ermitteln
+//Da kann man php code laufen lassen
+//http://phpfiddle.org/
+//Info ?er Errors
+//http://php.net/manual/de/function.error-reporting.php
+//hier hab ich die Boundingbox her
+//http://richardpeacock.com/sites/default/files/getDueCoords.php__0.txt
+
+
 
 $searchadress=$_GET['address'];   //Mindestparameter
-$distance= (isset($_GET['distance'])) ? $_GET['distance'] : 1; //Distancparameter ist optional in KM
+$distance= (isset($_GET['distance'])) ? $_GET['distance'] : 1.5; //Distancparameter ist optional in KM
 $valuetype= (isset($_GET['value'])) ? $_GET['value'] : "temperature"; //Welcher Sensortyp soll abgefragt werden
-$debug= (isset($_GET['debug'])) ? $_GET['debug'] : false; //Verbose
+$debug= (isset($_GET['debug'])) ? $_GET['debug'] : false; //Welcher Sensortyp soll abgefragt werden
 
 
  
@@ -33,6 +41,7 @@ if($debug){echo "Lat_SW:"  .$lat_sw . "<br>";}
 if($debug){echo "Lon_SW:"  .$lon_sw . "<br>";}
 if($debug){echo "Lat_NE:"  .$lat_ne . "<br>";}
 if($debug){echo "Lon_NE:"  .$lon_ne . "<br>";}
+if($debug){echo "Umkreissuche: "  .$distance . "km<br>";}
 
 //Netatmo Settings
 $username="";
@@ -73,19 +82,21 @@ $api_url = "https://api.netatmo.net/api/getpublicdata?access_token=" .  $params[
 $localweatherstations = file_get_contents($api_url);     //url abfragen
 $json_devices = json_decode($localweatherstations); //JSON generieren
 
-
 //Werte ermitteln
-echo getWeatherValue($json_devices,$valuetype,$debug); //Durchschnittswert ermitteln
+echo getWeatherValue($json_devices,$valuetype,$lat,$lng,$distance,$debug); //Durchschnittswert ermitteln
 if($debug){echo "<br>";}
-if($debug){drawGoogleMapsBoundry($lat,$lng,$distance);}
+
+if($debug){drawGoogleMapsBoundry($lat,$lng,$distance,getCoordsOfDevices($json_devices, $distance,$lat,$lng));}
 
 
 
-function getWeatherValue($json, $name,$debug){
+function getWeatherValue($json, $name,$lat,$lng,$dist,$debug){
    $dev_count=0;
    $temp_sum=0;
    foreach($json->body as $key => $devices)
    { //loop ?er einzelne Messstellen
+   if(circle_distance($devices->place->location[1],$devices->place->location[0],$lat,$lng)<$dist) //checkDistance
+   {	 
       foreach ($devices->measures as $key => $measures)
       { //einzelnen Messger?e
          if(isset($measures->type))
@@ -100,11 +111,12 @@ function getWeatherValue($json, $name,$debug){
                    //echo json_encode(($v[$x]), JSON_PRETTY_PRINT) . "<br>########<br>";
                   $temp_sum=$temp_sum+$v[$x];   //temperatur addieren
                   $dev_count++;  //Gefunden Messsensoren
-                  if($debug){echo $v[$x] . "<br>";}
+                  if($debug){echo "Sensor Cnt:". $dev_count ." Wert: " .$v[$x] . "<br>";}
                 }
               }
             }
          }
+      }
       }
    }
    if($debug){echo "Anzahl an Messstationen " . $dev_count . "<br>";}
@@ -140,29 +152,72 @@ function getDueCoords($latitude, $longitude, $bearing, $distance, $distance_unit
     return $coord;
 }	
 
+//Schnelle bestimmung
+function circle_distance($lat1, $lon1, $lat2, $lon2) {
+  $rad = M_PI / 180;
+  return acos(sin($lat2*$rad) * sin($lat1*$rad) + cos($lat2*$rad) * cos($lat1*$rad) * cos($lon2*$rad - $lon1*$rad)) * 6371;// Kilometers
+}
 
-function drawGoogleMapsBoundry($lat,$lng,$d){
+function getCoordsOfDevices($json, $dist,$lat,$lng){
+$marker="";
+foreach($json->body as $key => $devices)
+   { //loop ?er einzelne Messstellen
+   	if(circle_distance($devices->place->location[1],$devices->place->location[0],$lat,$lng)<$dist) //checkDistance
+   	{
+   		$marker=$marker.'&markers=size:small|color:orange|'.$devices->place->location[1].','.$devices->place->location[0];
+   	}	
+   }
+   return $marker;
+}
+
+function drawGoogleMapsBoundry($MapLat,$MapLng,$MapRadius,$marker){
 // Create the static map api image.
-    $static_maps_url = "http://maps.googleapis.com/maps/api/staticmap";
-    $static_maps_url .= "?center=$lat,$lng";
-    $static_maps_url .= "&zoom=13";
-    $static_maps_url .= "&size=300x300";
-    $static_maps_url .= "&maptype=roadmap";
-    $static_maps_url .= "&sensor=false";
-    $static_maps_url .= "&markers=color:blue|$lat,$lng";
 
-    // Figure out the corners of a box surrounding our lat/lng.
-    $path_top_right = getDueCoords($lat, $lng, 45, $d,"km",false);
-    $path_bottom_right = getDueCoords($lat, $lng, 135, $d,"km",false);
-    $path_bottom_left = getDueCoords($lat, $lng, 225, $d,"km",false);
-    $path_top_left = getDueCoords($lat, $lng, 315, $d,"km",false);
-    
-    $static_maps_url .= "&path=color:334433|weight:5|fillcolor:0xFFFF0033|";
-    $static_maps_url .= "$path_top_left|$path_top_right|$path_bottom_right|";
-    $static_maps_url .= "$path_bottom_left|$path_top_left";
-     
-    // Now, draw the image from Google Maps API!
-    print "<img src='$static_maps_url'>"; 
+//$static_maps_url .= "&markers=color:blue|$MapLat,$MapLng";
+$MapFill   = 'E85F0E';    // fill colour of our circle
+$MapBorder = '91A93A';    // border colour of our circle
+$MapWidth  = 640;         // map image width (max 640px)
+$MapHeight = 480;         // map image height (max 640px)
+$MapCenterMarker="&markers=color:blue|$MapLat,$MapLng".$marker;
+/* create our encoded polyline string */
+$EncString = GMapCircle($MapLat,$MapLng, $MapRadius);
+
+
+/* put together the static map URL */
+$MapAPI = 'http://maps.google.com.au/maps/api/staticmap?';
+$MapURL = $MapAPI.'center='.$MapLat.','.$MapLng.'&size='.$MapWidth.'x'.$MapHeight.'&maptype=roadmap&path=fillcolor:0x'.$MapFill.'33%7Ccolor:0x'.$MapBorder.'00%7Cenc:'.$EncString.$MapCenterMarker.'&sensor=false';
+
+/* output an image tag with our map as the source */
+echo '<img src="'.$MapURL.'" />';
+}
+
+function GMapCircle($Lat,$Lng,$Rad,$Detail=8){
+ $R    = 6371;
+
+ $pi   = pi();
+
+ $Lat  = ($Lat * $pi) / 180;
+ $Lng  = ($Lng * $pi) / 180;
+ $d    = $Rad / $R;
+
+ $points = array();
+ $i = 0;
+
+ for($i = 0; $i <= 360; $i+=$Detail):
+   $brng = $i * $pi / 180;
+
+   $pLat = asin(sin($Lat)*cos($d) + cos($Lat)*sin($d)*cos($brng));
+   $pLng = (($Lng + atan2(sin($brng)*sin($d)*cos($Lat), cos($d)-sin($Lat)*sin($pLat))) * 180) / $pi;
+   $pLat = ($pLat * 180) /$pi;
+
+   $points[] = array($pLat,$pLng);
+ endfor;
+
+ require_once('PolylineEncoder.php'); //http://www.svennerberg.com/examples/polylines/PolylineEncoder.php.txt
+ $PolyEnc   = new PolylineEncoder($points);
+ $EncString = $PolyEnc->dpEncode();
+
+ return $EncString['Points'];
 }
 ?>
  
